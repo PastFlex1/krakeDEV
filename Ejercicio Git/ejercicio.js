bb436bbe27a289d32ba64ba1fc3c3c3556e30afa
Pastflex <past6867@gmail.com>
2024-08-06 18:24:38 -0500

    gif

--- Ejercicio Git/ejercicio.js
+++ Ejercicio Git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmimgSaluda;
+    cmimgSaluda = document.getElementById("saludo");
+    cmimgSaluda.src = "imagenes/club-penguin.gif";
     console.log('Ingresa a saludar');
 }
