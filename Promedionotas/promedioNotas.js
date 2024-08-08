1ad506c8095096353cf9095ac89fe70f2fa5a869
Pastflex <past6867@gmail.com>
2024-08-08 14:51:58 -0500

    promedios

--- Promedionotas/promedioNotas.js
+++ Promedionotas/promedioNotas.js
@@ -3,7 +3,15 @@ calcularPromedionotas = function(){
     let nota2 = recuperarFloat("txtNota2");
     let nota3 = recuperarFloat("txtNota3");
 
-    let promedio = calcularPromedio(nota1,nota2,nota3);
-    return console.log(promedio);
+    let promedio =  calcularPromedio(nota1,nota2,nota3);
+    mostrarTexto("lblPromedio",promedio);
+    let promedioConDecimales = promedio.toFixed(2);
+    if(promedioConDecimales > 7){
+        mostrarImagen("Imagenfeliz","../Ejercicio Git/imagenes/Animated-GIF-Banana.gif");
+    }else{
+        mostrarImagen("Imagenfeliz","../Ejercicio Git/imagenes/sad.gif")
+    }
+    return console.log(promedioConDecimales);
+
 
 }
