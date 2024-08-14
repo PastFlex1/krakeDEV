902baf2c7a1cfea81d2011a4b16ea475e06aae13
Pastflex <past6867@gmail.com>
2024-08-13 19:49:12 -0500

    notas v2

--- Promedionotas/promedioNotas.js
+++ Promedionotas/promedioNotas.js
@@ -3,13 +3,22 @@ calcularPromedionotas = function(){
     let nota2 = recuperarFloat("txtNota2");
     let nota3 = recuperarFloat("txtNota3");
 
+
     let promedio =  calcularPromedio(nota1,nota2,nota3);
     mostrarTexto("lblPromedio",promedio);
     let promedioConDecimales = promedio.toFixed(2);
-    if(promedioConDecimales > 7){
-        mostrarImagen("Imagenfeliz","../Ejercicio Git/imagenes/Animated-GIF-Banana.gif");
+    if(promedio < 5 && promedio > 0){
+        mostrarImagen("Imagenfeliz","gif/reprobado.gif");
+        mostrarTexto("lblR","REPROBADO!!")
+    }else if(promedio >= 5 && promedio <= 8){
+        mostrarImagen("Imagenfeliz","gif/buentrabajo.gif");
+        mostrarTexto("lblR","BUEN TRABAJO!!")
+    }else if(promedio > 8 && promedio >= 10){
+        mostrarImagen("Imagenfeliz","gif/excelente.gif");
+        mostrarTexto("lblR","EXCELENTE!!")
     }else{
-        mostrarImagen("Imagenfeliz","../Ejercicio Git/imagenes/sad.gif")
+        mostrarImagen("Imagenfeliz","gif/error.gif");
+        mostrarTexto("lblR","ERROR!!")
     }
     return console.log(promedioConDecimales);
 
